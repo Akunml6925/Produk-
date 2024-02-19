e27fdda0fd0af053f58938f2fb26026a661e121b
akunml6925@gmail.com <akunml6925@gmail.com>
2024-02-19 11:22:05 +0700

    mencari elor

---
+++ main.js
@@ -0,0 +1,44 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  getDocs,
+  getDoc,
+  deleteDoc,
+  doc,
+  query,
+  orderBy,
+  updateDoc
+} from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA-wvBGzlYI9NHjVZBq7wbUHtEWrN3AFI8",
+  authDomain: "pasarbarokah-56d6c.firebaseapp.com",
+  projectId: "pasarbarokah-56d6c",
+  storageBucket: "pasarbarokah-56d6c.appspot.com",
+  messagingSenderId: "316348641371",
+  appId: "1:316348641371:web:5ad38a561e7d73744acf7e",
+};
+
+// Inisialisasi Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export async function ambilDaftarproduk(){
+  const refDokumen = collection(db, "produk");
+  const kuery = query(refDokumen, orderBy("nama"));
+  const cuplikankuery = await getDocs(kuery);
+  
+  let hasil = [];
+  cuplikankuery.forEach((dok) => {
+    hasil.push({
+      id: dok.id,
+      nama: dok.data().nama,
+      harga: dok.data().harga,
+      stok: dok.data().stok,
+    });
+  });
+  
+  return hasil;
+}
